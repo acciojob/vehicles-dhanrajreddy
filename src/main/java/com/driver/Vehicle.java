1476a8245fd9168bc4cf786ac0129072fd753c68
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-24 13:40:00 +0530

    codeAdded

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -31,4 +31,11 @@ public class Vehicle {
         System.out.println("stop method called - The vehicle is stopped");
     }
 
+    public int getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public int getCurrentDirection(){
+        return currentDirection;
+    }
 }
